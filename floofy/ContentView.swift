b4858cca33af7f7973cb8ce57b57d32be6542d94
baraboy <rizky.dwiixg36@gmail.com>
2023-05-27 15:33:13 +0700

    Initial Commit

---
+++ floofy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  floofy
+//
+//  Created by Rizky Dwi Hadisaputro on 27/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ floofy/floofyApp.swift
@@ -0,0 +1,17 @@
+//
+//  floofyApp.swift
+//  floofy
+//
+//  Created by Rizky Dwi Hadisaputro on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct floofyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
