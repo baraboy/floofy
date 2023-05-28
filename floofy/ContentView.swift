dddb4012375e6baa5f22db85f237e979811213ea
baraboy <rizky.dwiixg36@gmail.com>
2023-05-28 11:07:37 +0700

    TabBar View HIG

--- floofy/ContentView.swift
+++ floofy/ContentView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab: Int = 0
+    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        TabView(selection: $selectedTab){
+            Text("Scan Tab")
+                .tabItem{
+                    Image(systemName: "camera.viewfinder")
+                    Text("Scan")
+                }
+                .tag(0)
+            Text("Reminder Tab")
+                .tabItem{
+                    Image(systemName: "bell")
+                    Text("Reminder")
+                }
+                .tag(1)
+            Text("Challenges Tab")
+                .tabItem{
+                    Image(systemName: "gamecontroller")
+                    Text("Challenges")
+                }
+                .tag(2)
+            Text("Articles Tab")
+                .tabItem{
+                    Image(systemName: "newspaper")
+                    Text("Articles")
+                }
+                .tag(3)
+            Text("Pet Tab")
+                .tabItem{
+                    Image(systemName: "pawprint")
+                    Text("Pet")
+                }
         }
-        .padding()
     }
 }
 
